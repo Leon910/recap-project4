bfb1fc7653091f8b2e81ec82074843181642866d
Leon Fricke-Kleefisch <leonkleefisch@gmail.com>
2024-08-16 18:38:51 +0200

    add comments to most of the code to better comprehend for others

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
+/*  imports the CSS styles specific to the App component */
 import "./App.css";
+/* imports the ColorCard component */
 import ColorCard from "./Components/Color/Color";
+/* imports the initialColors array, which contains a list of color objects */
 import { initialColors } from "./lib/colors";
+/* imports the ColorForm component, which provides a form to add new colors */
 import ColorForm from "./Components/Color/ColorForm";
 
+/* define the structure of the UI */
 function App() {
   return (
+    /* fragments to group multiple elements without adding extra nodes to the DOM */
     <>
       <h1>Theme Creator</h1>
       <ColorForm />
+      {/* Renders an unordered list without default bullet points */}
       <ul style={{ listStyleType: "none" }}>
         <li>
+          {/* iterate over the initialColors array and renders a ColorCard for each color object */}
           {initialColors.map((color) => {
+            /* color={color} passes the color object as a prop to the ColorCard component */
             return <ColorCard key={color.id} color={color} />;
           })}
         </li>
--- src/Components/Color/ColorForm.jsx
+++ src/Components/Color/ColorForm.jsx
@@ -1,18 +1,27 @@
+/* add state to the functional components */
 import { useState } from "react";
+
+/* import a CSS file for styling the ColorForm component */
 import "./ColorForm.css";
 
+/* implement a ColorForm component with form and three input fields */
 export default function ColorForm() {
+  /* set state with default values to later be able to change the values */
   const [role, setRole] = useState("some color");
   const [hex, setHex] = useState("#125413");
   const [contrastText, setContrastText] = useState("#ffffff");
 
+  /* event-handler to submit the events */
   function handleSubmit(event) {
     event.preventDefault();
     console.log("submit works");
   }
 
+  /* button click handler - not yet finished */
   function handleClick() {}
 
+  /* input elements are tied to state via the value attribute with the name e.g. "role" */
+  /* onChange updates the corresponding state when the user types into the input field */
   return (
     <form onSubmit={handleSubmit} className="form-flex">
       <label htmlFor="role">Role</label>
