1e96916bda0caab7d82526f8b05c5a7ffcc91058
Leon Fricke-Kleefisch <leonkleefisch@gmail.com>
2024-08-21 12:13:16 +0200

    add Timeout effect and add success message

--- src/Components/CopyToClipboard.jsx
+++ src/Components/CopyToClipboard.jsx
@@ -1,13 +1,36 @@
+import { useState, useEffect } from "react";
+
 export default function CopyToClipboard({ copiedText }) {
+  const [isCopy, setIsCopy] = useState(false);
   /* handle the click event for copy button */
   const handleCopyClick = async () => {
     try {
       await navigator.clipboard.writeText(copiedText);
+
+      setIsCopy(true);
     } catch (error) {
-      console.error("Copy failed");
+      console.error("Copy failed", error);
     }
   };
-  handleCopyClick();
 
-  return <button onClick={handleCopyClick}>COPY</button>;
+  useEffect(() => {
+    if (isCopy) {
+      const timer = setTimeout(() => {
+        setIsCopy(false);
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [isCopy]);
+
+  return (
+    <div>
+      <button onClick={handleCopyClick}>COPY</button>
+      {isCopy && (
+        <p style={{ color: "white", backgroundColor: "#1bff3d" }}>
+          SUCCESSFULLY COPIED!
+        </p>
+      )}
+    </div>
+  );
 }
