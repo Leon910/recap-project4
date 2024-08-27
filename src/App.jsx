3ead5f8ce04ba744f9d6940bc1f2a82d84ff6318
Leon Fricke-Kleefisch <leonkleefisch@gmail.com>
2024-08-27 12:46:51 +0200

    remove comments fromm app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
-/*  imports the CSS styles specific to the App component */
 import "./App.css";
 import "./Components/Color/Color.css";
 import { initialThemes } from "./lib/themes";
 import { useState } from "react";
-
-/* imports the ColorCard component */
 import ColorCard from "./Components/Color/ColorCard";
-/* imports the initialColors array, which contains a list of color objects */
 import { initialColors } from "./lib/colors";
-/* imports the ColorForm component, which provides a form to add new colors */
 import ColorForm from "./Components/Color/ColorForm";
-/* imports the Analytics component functionalities from vercel */
-/* import { Analytics } from "@vercel/analytics/react"; */
-
 import useLocalStorageState from "use-local-storage-state";
 
-/* define the structure of the UI */
 export default function App() {
-  /* 
-  Declares a state variable 'colors' initialized with 'initialColors'.
-  'setColors' is a function used to update the 'colors' state.
-  */
-
   const [themes, setThemes] = useLocalStorageState("themes", {
     defaultValue: initialThemes,
   });
@@ -38,12 +24,6 @@ export default function App() {
     currentTheme.colors.includes(color.id)
   );
 
-  /*
-  Function to add a new color to the list of colors.
-  It updates the 'colors' state by adding the new color at the beginning of the array.
-  */
-
-  /* add-color logic not yet finished */
   const addColor = (newColor) => {
     setColors([newColor, ...colors]);
 
@@ -57,16 +37,12 @@ export default function App() {
     setThemes(updatedThemes);
   };
 
-  /* Function to handle the deletion of a color */
-  /* callback function "color => color.id" is applied to each element (color) in the colors array. */
-  /* checks if the id of the current color is not equal to the colorId passed to handleDelete. */
   const handleDelete = (colorId) => {
     setColors(colors.filter((color) => color.id !== colorId));
   };
 
   const handleColorUpdate = (updatedColor) => {
     setColors(
-      /** checks if the current color's id matches the id of the updatedColor (the color that has been edited) */
       colors.map((color) =>
         color.id === updatedColor.id ? updatedColor : color
       )
@@ -75,12 +51,12 @@ export default function App() {
 
   return (
     <ThemeDisplay
+      themes={themes}
       colors={themeColors}
       addColor={addColor}
       handleDelete={handleDelete}
       handleColorUpdate={handleColorUpdate}
       currentThemeId={currentThemeId}
-      themes={themes}
       setCurrentThemeId={setCurrentThemeId}
     />
   );
